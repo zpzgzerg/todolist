1e5907148546e27c15a5b1619d260fa29a02babe
zpzgzerg <zpzgzerg@naver.com>
2020-06-09 00:34:42 +0900

    todoapp 컴포넌트 기능 1차 구현 완료

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -18,8 +18,11 @@
     methods: {
       addTodo: function () {
         //console.log(this.newTodoItem);
-        localStorage.setItem(this.newTodoItem, this.newTodoItem);
-        this.clearInput();
+        if(this.newTodoItem !== '') {
+          var obj = { completed: false, item: this.newTodoItem};
+          localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+          this.clearInput();
+        }
       },
       clearInput: function() {
         this.newTodoItem = '';
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
     </ul>
   </div>
 </template>
@@ -13,11 +19,25 @@
         todoItems: []
       }
     },
+    methods: {
+      removeTodo: function (todoItem, index) {
+        console.log(todoItem, index);
+        localStorage.removeItem(todoItem.item);
+        this.todoItems.splice(index, 1);
+      },
+      toggleComplete: function (todoItem, index) {
+        console.log(todoItem, index);
+        todoItem.completed = !todoItem.completed
+        localStorage.removeItem(todoItem.item);
+        localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+      }
+    },
     created: function() {
       if(localStorage.length > 0) {
         for (var i = 0; i < localStorage.length; i++) {
-          if(localStorage.key(i) != 'loglevel:webpack-dev-server') {
-            this.todoItems.push(localStorage.key(i));
+          if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+            console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
           }
         }
       }
@@ -26,5 +46,36 @@
 </script>
 
 <style scoped>
-
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .checkBtnCompleted {
+    color: #b3adad;
+  }
+  .textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+  }
 </style>
