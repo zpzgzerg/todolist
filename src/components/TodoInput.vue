747355479b7b57eaca8036018e0456c3c828c04b
zpzgzerg <zpzgzerg@naver.com>
2020-06-09 23:37:40 +0900

    todoapp 컴포넌트 기능 최종 구현 완료

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -5,28 +5,46 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="fas fa-plus addBtn"></i>
     </span>
+    <Modal v-if="showModal" @close="showModal = false">
+      <!--
+        you can use custom content here to overwrite
+        default content
+      -->
+      <h3 slot="header">
+        경고!
+        <i class="fas fa-times closeModalBtn" @click="showModal = false"></i>
+      </h3>
+      <div slot="body">아무것도 입력하지 않았습니다.</div>
+    </Modal>
   </div>
 </template>
 
 <script>
+  import Modal from './common/Modal.vue'
+
   export default {
     data: function () {
       return {
-       newTodoItem: ""
+        newTodoItem: "",
+        showModal: false
       }
     },
     methods: {
       addTodo: function () {
         //console.log(this.newTodoItem);
         if(this.newTodoItem !== '') {
-          var obj = { completed: false, item: this.newTodoItem};
-          localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+          this.$emit('addTodoItem',this.newTodoItem);
           this.clearInput();
+        } else {
+          this.showModal = !this.showModal;
         }
       },
       clearInput: function() {
         this.newTodoItem = '';
       }
+    },
+    components: {
+      'Modal': Modal
     }
   }
 </script>
@@ -55,5 +73,8 @@
   .addBtn {
     color: white;
     vertical-align: middle;
+  }
+  .closeModalBtn {
+    color: #42b983;
   }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,45 +1,30 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+    <transition-group name="list" tag="ul">
+      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
         <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
         <span v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
 <script>
   export default {
-    data: function() {
-      return {
-        todoItems: []
-      }
-    },
+    props: ['propsdata'],
     methods: {
       removeTodo: function (todoItem, index) {
-        console.log(todoItem, index);
-        localStorage.removeItem(todoItem.item);
-        this.todoItems.splice(index, 1);
+        this.$emit('removeItem', todoItem, index);
+        //console.log(todoItem, index);
       },
       toggleComplete: function (todoItem, index) {
-        console.log(todoItem, index);
-        todoItem.completed = !todoItem.completed
+        this.$emit('toggleItem', todoItem, index);
+        /*todoItem.completed = !todoItem.completed
         localStorage.removeItem(todoItem.item);
-        localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
-      }
-    },
-    created: function() {
-      if(localStorage.length > 0) {
-        for (var i = 0; i < localStorage.length; i++) {
-          if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-            console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
-            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-          }
-        }
+        localStorage.setItem(todoItem.item, JSON.stringify(todoItem))*/
       }
     }
   }
@@ -77,5 +62,12 @@
   .textCompleted {
     text-decoration: line-through;
     color: #b3adad;
+  }
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(30px);
   }
 </style>
